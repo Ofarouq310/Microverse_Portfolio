34c234d0a5b4351b754301dc6b459b39d5ad5214
Omar Farouq <ofarouq310@gmail.com>
2022-10-13 22:43:04 +0200

    Code modified

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ for (let i = 0; i < menuItems.length; i += 1) {
 // dynamic preview-cards
 const cards = [{
   title: 'Multi-Post Stories',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
   img: 'Sources/Desktop/Snapshoot Portfolio.png',
   tech1: 'html',
   tech2: 'css',
